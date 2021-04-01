593738156db297b02bf0712e7eb3aa6df4f0a094
Robert Hahn <robert.hahn@mailbox.org>
2021-04-01 10:13:50 +0200

    Added StringExtensions

---
+++ Sources/SwiftExtensions/StringExtensions.swift
@@ -0,0 +1,34 @@
+import Foundation
+import CommonCrypto
+
+
+extension NSString {
+    static func randomString(length: Int, letters: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") -> String {
+        return String((0 ..< length).map{ _ in letters.randomElement()! })
+    }
+}
+
+
+extension String {
+    public enum TruncationPosition {
+        case head
+        case middle
+        case tail
+    }
+
+    public func truncated(limit: Int, position: TruncationPosition = .middle, leader: String = "...") -> String {
+        guard self.count > limit else { return self }
+
+        switch position {
+        case .head:
+            return leader + self.suffix(limit)
+        case .middle:
+            let headCharactersCount = Int(ceil(Float(limit) / 2.0))
+            let tailCharactersCount = Int(floor(Float(limit) / 2.0))
+            return "\(self.prefix(headCharactersCount))\(leader)\(self.suffix(tailCharactersCount))"
+        case .tail:
+            return self.prefix(limit) + leader
+        }
+    }
+}
+
