ff1ab8fee0043da6b356905df5e4769b19dd9bfc
Robert Hahn <robert.hahn@mailbox.org>
2024-05-23 12:12:46 +0200

    Images for regular NSMenuItems

--- Sources/SwiftExtensions/NSMenu+.swift
+++ Sources/SwiftExtensions/NSMenu+.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Robert Hahn on 22.10.23.
 //
@@ -14,8 +14,9 @@ extension NSMenuItem {
 			self.image = image
 		}
 	}
-	
-	public convenience init(title string: String, image: NSImage? = nil, action selector: Selector?) {
+
+	public convenience init(title string: String, image: NSImage? = nil, action selector: Selector?)
+	{
 		self.init(title: string, action: selector, keyEquivalent: "")
 		if let image {
 			self.image = image
@@ -24,26 +25,54 @@ extension NSMenuItem {
 }
 
 extension NSMenu {
-	public func addItem(withTitle string: String) {
+	public func addItem(
+		withTitle string: String,
+		image: NSImage? = nil
+	) {
 		let item = NSMenuItem(title: string, action: nil, keyEquivalent: "")
 		item.isEnabled = false
+		if let image {
+			item.image = image
+		}
 		self.addItem(item)
 	}
-	
-	public func addItem(withTitle string: String, action selector: Selector?, target: AnyObject? = nil) {
+
+	public func addItem(
+		withTitle string: String,
+		image: NSImage? = nil,
+		action selector: Selector?,
+		target: AnyObject? = nil
+	) {
 		let item = NSMenuItem(title: string, action: selector, keyEquivalent: "")
+		if let image {
+			item.image = image
+		}
 		item.target = target
 		self.addItem(item)
 	}
-	
-	public func addItem(withTitle string: String, image: NSImage? = nil, state: Bool? = nil, keyEquivalent: String = "", closure action: @escaping (NSMenuItem) -> Void) {
+
+	public func addItem(
+		withTitle string: String,
+		image: NSImage? = nil,
+		state: Bool? = nil,
+		keyEquivalent: String = "",
+		closure action: @escaping (NSMenuItem) -> Void
+	) {
 		self.addItem(ActionMenuItem(title: string, image: image, state: state, closure: action))
 	}
-	
-	public func addItem(withTitle string: String, image: NSImage? = nil, state: Bool? = nil, keyEquivalent: String = "", closure action: @escaping () -> Void) {
-		self.addItem(ActionMenuItem(title: string, image: image, state: state, closure: { _ in action() }))
+
+	public func addItem(
+		withTitle string: String,
+		image: NSImage? = nil,
+		state: Bool? = nil,
+		keyEquivalent: String = "",
+		closure action: @escaping () -> Void
+	) {
+		self.addItem(
+			ActionMenuItem(title: string, image: image, state: state, closure: { _ in action() })
+		)
 	}
-	
+
 	public func addSeparator() {
 		self.addItem(NSMenuItem.separator())
 	}
@@ -53,7 +82,7 @@ extension NSMenu {
 
 public class ActionMenuItem: NSMenuItem {
 	private var closure: ((NSMenuItem) -> Void)?
-	
+
 	public init(
 		title: String,
 		keyEquivalent: String = "",
@@ -63,7 +92,7 @@ public class ActionMenuItem: NSMenuItem {
 		self.closure = closure
 		self.target = self
 	}
-	
+
 	public convenience init(
 		title: String,
 		image: NSImage? = nil,
@@ -79,13 +108,13 @@ public class ActionMenuItem: NSMenuItem {
 			self.state = state ? .on : .off
 		}
 	}
-	
+
 	required init(
 		coder: NSCoder
 	) {
 		fatalError("init(coder:) has not been implemented")
 	}
-	
+
 	@objc func onClick(sender: NSMenuItem) {
 		closure?(sender)
 	}
