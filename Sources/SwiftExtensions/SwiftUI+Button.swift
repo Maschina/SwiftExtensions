bb7d45b615c5b80f5bbb12b7c9d11e51d829243c
Robert Hahn <robert.hahn@mailbox.org>
2024-11-11 11:43:37 +0100

    SwiftUI extensions for Buttons

---
+++ Sources/SwiftExtensions/SwiftUI+Button.swift
@@ -0,0 +1,58 @@
+//
+//  SwiftUI+Button.swift
+//  SwiftExtensions
+//
+//  Created by Robert Hahn on 11.11.24.
+//
+
+import SwiftUI
+
+extension Button where Label == Image {
+	public init(systemImage: String, action: @escaping () -> Void) {
+		self.init {
+			action()
+		} label: {
+			Image(systemName: systemImage)
+		}
+	}
+}
+
+extension Button where Label == Text {
+	public init(_ titleKey: LocalizedStringKey, action: @escaping @MainActor () async -> Void) {
+		self.init(titleKey) {
+			Task {
+				await action()
+			}
+		}
+	}
+	
+	public init(_ titleKey: LocalizedStringKey, role: ButtonRole?, action: @escaping @MainActor () async -> Void) {
+		self.init(titleKey, role: role) {
+			Task {
+				await action()
+			}
+		}
+	}
+}
+
+extension Button {
+	public init(action: @escaping @MainActor () async -> Void, @ViewBuilder label: () -> Label) {
+		self.init {
+			Task {
+				await action()
+			}
+		} label: {
+			label()
+		}
+	}
+}
+
+extension Button where Label == SwiftUI.Label<Text, Image> {
+	public init(_ titleKey: LocalizedStringKey, systemImage: String, action: @escaping @MainActor () async -> Void) {
+		self.init(titleKey, systemImage: systemImage) {
+			Task {
+				await action()
+			}
+		}
+	}
+}
